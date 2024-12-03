84e14c6817b605507cb8e9204d5699290bb1f0dd
kiraTSG <102935872+kiraTSG@users.noreply.github.com>
2024-12-03 08:01:12 -0300

    Add files via upload

---
+++ trabalhoVaca/fazenda/fazenda/src/main/java/modelo/VacaProducao.java
@@ -0,0 +1,53 @@
+
+package modelo;
+
+import java.time.LocalDate;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
+public class VacaProducao {
+
+    @BsonProperty(value="brinco") // unico para cada vaca 
+    private Vaca brinco;
+    
+    @BsonProperty(value="data")
+    private LocalDate data;
+    
+    @BsonProperty(value="leite")
+    private String leite; 
+
+    public VacaProducao(){
+        brinco = new Vaca(); 
+        data = LocalDate.now();
+        leite = "";
+    }
+    
+    public VacaProducao(Vaca brinco, LocalDate data, String leite){
+        this.brinco = brinco; 
+        this.data = data;
+        this.leite = leite;
+    }
+
+    public Vaca getBrinco() {
+        return brinco;
+    }
+
+    public void setBrinco(Vaca brinco) {
+        this.brinco = brinco;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getLeite() {
+        return leite;
+    }
+
+    public void setLeite(String leite) {
+        this.leite = leite;
+    }
+}
